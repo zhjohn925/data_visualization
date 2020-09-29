10e88944cdfda8e03aa5c20fc4f4294e0c101898
zhjohn925 <zhjohn925@gmail.com>
2020-09-29 16:09:39 -0700

    Update intro_pandas.py

--- intro_pandas.py
+++ intro_pandas.py
@@ -1,3 +1,14 @@
+
+################################################################################################
+# pandas is an essential data analysis toolkit for Python.
+# pandas is a Python package providing fast, flexible, and expressive data structures 
+#     designed to make working with “relational” or “labeled” data both easy and intuitive. 
+#     It aims to be the fundamental high-level building block for doing practical, real world 
+#     data analysis in Python.
+# pandas API Reference: 
+#     http://pandas.pydata.org/pandas-docs/stable/api.html.
+################################################################################################
+
 import numpy as np  # useful for many scientific computing in Python
 import pandas as pd # primary data structure library
 
